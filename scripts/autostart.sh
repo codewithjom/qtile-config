c1d54186f63b8ab8a81987dea826bf9f56764327
Jom Dollesin <gitnightjd@gmail.com>
2022-04-06 22:50:12 +0800

    Sets new wallpaper.

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,4 +4,4 @@ conky -c $HOME/.config/qtile/scripts/conkyrc &
 sxhkd -c ~/.config/qtile/scripts/sxhkdrc &
 pamac-tray &
 blueberry-tray &
-nitrogen --set-scaled ~/.config/qtile/scripts/wall.jpg &
+nitrogen --set-scaled ~/.config/qtile/scripts/wall.png &
